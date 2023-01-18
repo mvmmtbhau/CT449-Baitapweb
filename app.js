e12a27b44342c84500d8845233b30247432bb90e
mvmmtbhau <nguyen1hau2211@gmail.com>
2023-01-18 14:05:41 +0700

    Cai dat cac handler truy xuat CSDL

--- app.js
+++ app.js
@@ -1,14 +1,13 @@
 const express = require('express');
 const cors = require('cors');
 const ApiError = require('./app/api-error');
+const contactsRouter = require('./app/routes/contact.route');
 
 const app = express();
 
-const contactsRouter = require('./app/routes/contact.route');
-
 app.use(cors());
 app.use(express.json());
-app.use('api/contacts', contactsRouter);
+app.use('/api/contacts', contactsRouter);
 
 // Middleware xử lý lỗi
 app.use((req, res, next) => {
--- app/services/contact.service.js
+++ app/services/contact.service.js
@@ -1,4 +1,4 @@
-const { objectId } = require('mongodb');
+const { ObjectId } = require('mongodb');
 
 class ContactService {
     constructor(client) {
@@ -6,6 +6,77 @@ class ContactService {
     }
 
     // Định nghĩa các phương thức truy xuất database
+    extraContactData(payload) {
+        const contact = {
+            name: payload.name,
+            email: payload.email,
+            address: payload.address,
+            phone: payload.phone,
+            favorite: payload.favorite,
+        };
+        // remove undefined fields
+        Object.keys(contact).forEach(
+            (key) => contact[key] === undefined && delete contact[key]
+        );
+        return contact;
+    }
+
+    async create(payload) {
+        const contact = this.extraContactData(payload);
+        const result = await this.Contact.findOneAndUpdate(
+            contact,
+            { $set: { favorite: contact.favorite === true } },
+            { returnDocument: "after", upsert: true },
+        );
+        return result.value;
+    }
+
+    async find(filter) {
+        const cursor = await this.Contact.find(filter);
+        return await cursor.toArray();
+    }
+
+    async findByName(name) {
+        return await this.find({
+            name: { $regex: new RegExp(name), $options: "i" },
+        });
+    }
+
+    async findById(id) {
+        return await this.Contact.findOne({
+            _id: ObjectId.isValid(id) ? new ObjectId(id) : null,
+        });
+    }
+
+    async findFavorite(){
+        return await this.find({ favorite: true});
+    }
+
+    async update(id, payload) {
+        const filter = {
+            _id: ObjectId.isValid(id) ? new ObjectId(id) : null,
+        };
+
+        const update = this.extraContactData(payload);
+        const result = await this.Contact.findOneAndUpdate(
+            filter,
+            {$set: update},
+            {returnDocuemnt: "after"},
+        );
+        return result.value;
+    }
+
+    async delete(id) {
+        const result = await this.Contact.findOneAndDelete({
+            _id: ObjectId.isValid(id) ? new ObjectId(id) : null,
+        });
+        return result.value;
+    }
+
+    async deleteAll() {
+        const result = await this.Contact.deleteMany({});
+        return result.deletedCount;
+    }
 }
 
 module.exports = ContactService;
